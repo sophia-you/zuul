b6d46b71c8cc98f646304edcddb95e933798aa4c
U-BSD\418807 <418807@SHS-BYBF6X3.beaverton.k12.or.us>
2023-11-30 09:14:53 -0800

    created rooms

--- main.cpp
+++ main.cpp
@@ -1,9 +1,96 @@
 #include <iostream>
 #include <map>
 #include <cstring>
+#include "room.h"
 using namespace std;
 
 int main()
 {
-  cout << "hello" << endl;
+  // DIRECTIONS
+  char* north = new char[100];
+  char* south = new char[100];
+  char* east = new char[100];
+  char* west = new char[100];
+  strcpy(north, "north");
+  strcpy(south, "south");
+  strcpy(east, "east");
+  strcpy(west, "west");
+
+  // ROOMS AND ROOM DESCRIPTIONS
+
+  // chow mein noodle house
+  char* chowmeinDescription = new char[100];
+  strcpy(chowmeinDescription, "You are in the Chow Mein noodle house.");
+  Room* chowmein = new Room(chowmeinDescription);
+
+  // rice cake stall
+  char* ricecakeDescription = new char[100];
+  strcpy(ricecakeDescription, "You have arrived at the rice cake food stall.");
+  Room* ricecake = new Room(ricecakeDescription);
+
+  // ramen restaurant
+  char* ramenDescription = new char[100];
+  strcpy(ramenDescription, "You have walked inside the ramen restaurant.");
+  Room* ramen = new Room(ramenDescription);
+
+  // sesame ball stall
+  char* sesameDescription = new char[100];
+  strcpy(sesameDescription, "You have arrived at the sesame ball food stall.");
+  Room* sesame = new Room(sesameDescription);
+
+  // pancake stall
+  char* pancakeDescription = new char[100];
+  strcpy(pancakeDescription, "You have arrived at the pancake food stall.");
+  Room* pancake = new Room(pancakeDescription);
+
+  // spring roll stall
+  char* springrollDescription = new char[100];
+  strcpy(springrollDescription, "You have arrived at the spring roll food stall.");
+  Room* springroll = new Room(springrollDescription);
+
+  // wonton stall
+  char* wontonDescription = new char[100];
+  strcpy(wontonDescription, "You have arrived at the wonton food stall.");
+  Room* wonton = new Room(wontonDescription);
+
+  // pho restaurant
+  char* phoDescription = new char[100];
+  strcpy(phoDescription, "You have entered the pho restuarant.");
+  Room* pho = new Room(phoDescription);
+
+  // central park
+  char* parkDescription = new char[100];
+  strcpy(parkDescription, "You have stepped outside into Central Park, the center of the Dragon's Market. Wild animals roam free in the bamboo forest.");
+  Room* park = new Room(parkDescription);
+
+  // udon restaurant
+  char* udonDescription = new char[100];
+  strcpy(udonDescription, "You have entered the Grand Udon Restaurant.");
+  Room* udon = new Room(udonDescription);
+
+  // tofu palace
+  char* tofuDescription = new char[100];
+  strcpy(tofuDescription, "You are in the largest restaurant at the Dragon's Market, Tofu Palace.");
+  Room* tofu = new Room(tofuDescription);
+
+  // entrance
+  char* entranceDescription = new char[100];
+  strcpy(ricecakeDescription, "You stand at the entrance of the Dragon's Market.");
+  Room* entrance = new Room(entranceDescription);
+
+  // dumpling house
+  char* dumplingDescription = new char[100];
+  strcpy(dumplingDescription, "You have arrived at the most popular restaurant in the Market, the Dumpling House.");
+  Room* dumpling = new Room(dumplingDescription);
+
+  // bathroom
+  char* bathroomDescription = new char[100];
+  strcpy(bathroomDescription, "You have entered a small, dirty room with an old toilet. It is the bathroom.");
+  Room* bathroom = new Room(bathroomDescription);
+
+  // bakery
+  char* bakeryDescription = new char[100];
+  strcpy(bakeryDescription, "You have arrived at the Fresh Bread Bakery, which sells a variety of aromatic baked goods.");
+  Room* bakery = new Room(bakeryDescription);
+
 }
--- room.cpp
+++ room.cpp
@@ -2,4 +2,38 @@
 #include <map>
 #include <cstring>
 #include <vector>
+#include "room.h"
 using namespace std;
+
+Room::Room() // default constructor
+{
+  
+}
+
+Room::Room(char* newdescription) // constructor with description passed in
+{
+  description = new char[100];
+  strcpy(description, newdescription);
+}
+
+Room::~Room() // destructor
+{
+  delete description;
+}
+
+void Room::setExit(char* direction, Room* nextRoom) // set exits of a room
+{
+  // direction = n/s/e/w and nextRoom = destination
+  exits.insert(pair<char*, Room*>(direction, nextRoom));
+}
+
+void Room::printExits() // prints all possible exits
+{
+  // walks through the map of rooms
+  for (map<char*, Room*>::iterator it = exits.begin(); it != exits.end(); it++)
+    {
+      cout << it->first << endl; // prints out the directions
+    }
+}
+
+
